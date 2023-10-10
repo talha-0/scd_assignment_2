11437cddfdd30b0c5001cded380a5f983c87ac70
Talha Khan <mtalhak93@gmail.com>
2023-10-11 00:43:38 +0500

    Added the stream queue for q2

--- src/datastructures/FirstNonRepeatingStream.java
+++ src/datastructures/FirstNonRepeatingStream.java
@@ -1,22 +1,30 @@
 package datastructures;
 
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class FirstNonRepeatingStream {
     char count[] = new char[256];
     HashMap<Integer, Character> non_repeating_char;
-
+    Queue<Character> queue;
     public FirstNonRepeatingStream(){
+        queue = new LinkedList<>();
         for (int i = 0; i < 256; i++)
             count[i] = 0;
         non_repeating_char = new HashMap<>();
     }
     public void add(char c){
+        queue.add(c);
         count[(int) c]++;
         if (count[c]==2)
             non_repeating_char.remove((int)c);
         else if (count[c]==1)
             non_repeating_char.put((int) c,c);
+    }
+
+    public void printQueue(){
+        System.out.println("Stream: "+queue);
     }
 
     public char getFirstNonRepeatingChar(){
--- src/q2.java
+++ src/q2.java
@@ -6,13 +6,16 @@ public class q2 {
         stream.add('a');
         stream.add('b');
         stream.add('a');
+        stream.printQueue();
         System.out.println("First non-repeating character: " + stream.getFirstNonRepeatingChar());
         stream.add('b');
+        stream.printQueue();
         System.out.println("First non-repeating character: " + stream.getFirstNonRepeatingChar());
         FirstNonRepeatingStream stream2 = new FirstNonRepeatingStream();
         stream2.add('a');
         stream2.add('b');
         stream2.add('b');
+        stream2.printQueue();
         System.out.println("First non-repeating character: " + stream2.getFirstNonRepeatingChar());
     }
 }
